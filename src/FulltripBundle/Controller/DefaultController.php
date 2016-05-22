5af1da25483ef47982ae2eaca4cabe051542e642
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-05-22 20:32:02 +0200

    Add search on homepage

--- src/FulltripBundle/Controller/DefaultController.php
+++ src/FulltripBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace FulltripBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use FulltripBundle\Form\SearchType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FulltripBundle:Default:index.html.twig');
+        $form = $this->createForm(SearchType::class);
+        return $this->render('FulltripBundle:Default:index.html.twig', array('form' => $form->createView()));
     }
 }
